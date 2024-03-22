b39db1811b99c7efceefdde93a6e89867158f493
ESHWARGUNDLA <eshwargundla2001@gmail.com>
2024-03-22 11:43:31 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Logo({width="40%"}) {
+  return (
+     <img src="https://tse1.mm.bing.net/th?id=OIP.miOBWRi1ajApC5zOWzojRAHaFs&pid=Api&P=0&h=180" style={{width}} alt="Logo"/>
+  );
+}
+
+export default Logo;
